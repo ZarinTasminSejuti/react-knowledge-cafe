667644f140489f97a1fe6639c22ce1900970d209
Zarin Tasmin Sejuti <zts-7234@hotmail.com>
2023-09-12 21:03:47 +0300

    added single blog component and prop types warning issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-
+import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-6xl bg-green-500 font-bold'>Knowledge Cafe</h1>
-        </div>
+        <header className='flex justify-between items-center p-5 mx-4 border-b-2'>
+            <h1 className='text-4xl bg-green-500 font-bold'>Knowledge Cafe</h1>
+            <img src={profile}alt="" />
+        </header>
     );
 };
 
