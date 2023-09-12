fcc6cdbf7d50fa6c6a10cf5fa1e49df2332ce4e9
Zarin Tasmin Sejuti <zts-7234@hotmail.com>
2023-09-12 20:29:51 +0300

    add json file and set state

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl bg-green-500 font-bold'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
